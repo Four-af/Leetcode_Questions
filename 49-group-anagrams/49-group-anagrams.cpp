952df1283eaa8fc2528107038988c6b023f49c18
Four-af <83394414+Four-af@users.noreply.github.com>
2022-01-22 23:56:10 +0530

    Time: 36 ms (71.84%), Space: 21 MB (27.29%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map <string,vector<int>> mp{};      
+        
+        for(int i=0; i<strs.size(); i++){
+            string str = strs[i];
+            sort(str.begin(),str.end());
+            mp[str].push_back(i);
+        }
+        vector<vector<string>> res{};
+        for ( auto itr:mp){
+            vector<string> t{};
+            
+        for(int i=0; i<itr.second.size(); i++){
+            t.push_back(strs[itr.second[i]]);
+        }res.push_back(t);
+            
+        }return res;
+    }
+};
