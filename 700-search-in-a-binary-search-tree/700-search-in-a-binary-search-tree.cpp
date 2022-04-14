a2fb00adce0616adced70799cdea805e8c25ef9b
Four-af <83394414+Four-af@users.noreply.github.com>
2022-04-14 22:02:42 +0530

    Time: 42 ms (82.65%), Space: 34.8 MB (24.22%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+    TreeNode* dfs(TreeNode* root, int val) {
+        if(root==NULL) return NULL;
+    
+        if(root->val == val) {
+            return root;
+        }
+        if(root->val > val) {
+           return dfs(root->left, val);
+        } 
+        // if(root->val < val) {
+           return dfs(root->right, val);
+        // }
+        
+    }
+
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* ans = NULL;
+        
+        return dfs(root, val);;
+        
+    }
+};
