a8c00c118efc422faeaea111d015b8f992f54988
Four-af <83394414+Four-af@users.noreply.github.com>
2022-05-12 23:03:34 +0530

    Time: 7 ms (80.95%), Space: 8.4 MB (89.32%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end() );
-        cout << "The 3! possible permutations with 3 elements:\n";
+        // cout << "The 3! possible permutations with 3 elements:\n";
         vector<vector<int>> ress;
         int i=0;
         do {
@@ -12,12 +12,6 @@ public:
 
         return ress;
                  
-        // n = nums.size();
-        // sort(nums.begin(), nums.end());
-        // vector<vector<int>> ans; 
-        // ans.push_back(nums);
-        // while(next_permu(nums))
-        //     ans.push_back(nums);
-        // return ans;
+       
     }
 };
