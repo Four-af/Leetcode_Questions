9d280d6146d81aa1867b0b506171593635d9e352
Four-af <83394414+Four-af@users.noreply.github.com>
2022-06-14 14:58:02 +0530

    Time: 56 ms (14.10%), Space: 13 MB (51.65%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string t1, string t2) {
+        int n = t1.size();
+        int m = t2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(t1[i-1] == t2[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+
+
+//i-  0 1 2 3 4 5 6
+//j/0 0 1 0 0 0 0 0
+//|/1 0 0 0 0 0 0 0
+////2 0 0 1 0 0 0 0
+////3 
+////4
+////5
