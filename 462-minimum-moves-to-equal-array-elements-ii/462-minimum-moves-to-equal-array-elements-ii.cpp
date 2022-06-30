001920075e4a8ab4429f908ab560c54c5b37cdc9
Four-af <83394414+Four-af@users.noreply.github.com>
2022-06-30 19:15:50 +0530

    Time: 23 ms (20.71%), Space: 10.9 MB (15.75%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n = nums.size(), moves = 0;
+        sort(nums.begin(), nums.end());
+        int mid = nums[n/2];
+        for(int i=0; i<n; i++){
+            moves += abs(nums[i] - mid);
+        }
+        return moves;
+    }
+};
