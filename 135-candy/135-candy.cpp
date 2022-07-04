fa2cebd200a6383b05f5a8bca57184ed11b5490f
Four-af <83394414+Four-af@users.noreply.github.com>
2022-07-04 23:58:11 +0530

    Time: 37 ms (40.28%), Space: 17.8 MB (65.79%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   int candy(vector<int> &ratings)
+     {
+         int size=ratings.size();
+         if(size<=1)
+             return size;
+         vector<int> num(size,1);
+       //givin candy having higher rated guy more candy from left member
+         for (int i = 1; i < size; i++)
+         {
+             if(ratings[i]>ratings[i-1])
+                 num[i]=num[i-1]+1;
+         }
+       //givin candy having higher rated guy more candy from its  right member
+         for (int i= size-1; i>0 ; i--)
+         {
+             if(ratings[i-1]>ratings[i])
+                 num[i-1]=max(num[i]+1,num[i-1]);
+         }
+         int result=0;
+         for (int i = 0; i < size; i++)
+         {
+             result+=num[i];
+
+         }
+         return result;
+     }
+};
