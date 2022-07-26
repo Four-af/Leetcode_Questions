fdbac8a14356a8506bc374d07fae1f5fd9fdabce
Four-af <83394414+Four-af@users.noreply.github.com>
2022-07-27 01:48:15 +0530

    Time: 21 ms (70.31%), Space: 14.1 MB (96.96%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -26,22 +26,3 @@ public:
         return dfsTraverse(root, p, q);
     }
 };
-
-// class Solution {
-// public:
-// TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-// if(!root || root->val==p->val || root->val == q->val)
-// return root;
-
-//     TreeNode* left=lowestCommonAncestor(root->left,p,q);
-//     TreeNode* right=lowestCommonAncestor(root->right,p,q);
-
-//     if(!left)
-//         return right;
-//     else if(!right)
-//         return left;
-    
-//     return root;
-    
-// }
-// };
