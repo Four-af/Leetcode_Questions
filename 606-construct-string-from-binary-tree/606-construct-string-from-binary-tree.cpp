644b40622b19b3fa4b1c33a8bcad2b69709fc933
Four-af <83394414+Four-af@users.noreply.github.com>
2022-09-08 02:12:03 +0530

    Time: 77 ms (9.45%), Space: 54.6 MB (17.88%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -9,39 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-    
-//     void preorder(TreeNode* root, vector<int>&v, string s){
-//         if(root==NULL){
-//             return;
-//         }
-//         s+="(";
-//         s+=(char)root->val;
-//         v.push_back(root->val);
-//         if(root->left == NULL) s+="()";
-//             preorder(root->left,v,s);
-//         if(root->right == NULL) preorder(root->right,v,s);
-        
-//     }
-    
-//     string tree2str(TreeNode* root) {
-//         vector<int> v;
-//         string s;
-//         preorder(root,v,s);
-//     }
-// };
-
 class Solution {
 public:
-    string tree2str(TreeNode* root) {
-		string ans = to_string(root->val);
-		if (root->left) //left side check
-			ans += "(" + tree2str(root->left) + ")";
-		if (root->right) { //right side check
-			if (!root->left) ans += "()"; //left side not present, but right side present
-			ans += "(" + tree2str(root->right) + ")"; 
+    
+    string preorder(TreeNode* root,string si){
+        
+        if(root==NULL){
+            return si;
+        }
+        string s = to_string(root->val);
+		if (root->left)
+			s += "(" + preorder(root->left,s) + ")";
+		if (root->right) {
+			if (!root->left) s += "()";
+			s += "(" + preorder(root->right,s) + ")"; 
 		}
-		return ans;
-	}
+		return s;
+    }
+    
+    string tree2str(TreeNode* root) {
+        // vector<int> v;
+        string s="";
+        return preorder(root,s);
+    
+    }
 };
