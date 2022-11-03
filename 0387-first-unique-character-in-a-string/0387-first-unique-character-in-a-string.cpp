2bafb7db56a9a346e62c8208d43a73e0218fb90a
Four <83394414+Four-af@users.noreply.github.com>
2022-11-03 19:24:30 +0530

    Time: 56 ms (70.93%), Space: 11 MB (15.69%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        // vector<pair<int,int>> f(s.size());
+        vector<vector<int>> f(26,vector<int>(2,-1));
+        for(int i=0; i<s.size(); i++){
+            f[s[i]-'a'][0]++;
+            f[s[i]-'a'][1] = i;
+        }
+        vector<int> idx;
+        for(int i=0; i<26; i++){
+            // if(f[i][0] == -1) continue;
+            // else
+            if(f[i][0] == 0) idx.push_back(f[i][1]);
+            else continue;
+        }
+        if(idx.size()==0) return -1;
+        else{
+            return *min_element(idx.begin(), idx.end());
+        }
+    }
+};
