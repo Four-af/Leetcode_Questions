a29bff05dd33fb2557d48e4baf14525592226db5
Aiman <83394414+Four-af@users.noreply.github.com>
2022-11-27 23:47:35 +0530

    Time: 29 ms (73.58%), Space: 26.4 MB (27.64%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//same as post and inorder with hashmap but here
+//we do da left subtree then rightsubtree
+//rest recur logik iz sem
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int rootIdx = 0;
+        unordered_map<int,int> hmap;
+        for (int i=0; i< inorder.size(); i++) {
+            hmap[inorder[i]] = i;
+        }
+        return build(preorder, inorder, rootIdx, 0, inorder.size()-1, hmap);
+    }
+    
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int &rootIdx, int left, int right, unordered_map<int,int> &hmap) {
+        if(left > right) return nullptr;        
+        int pivot = hmap[preorder[rootIdx]];
+        TreeNode* node = new TreeNode(inorder[pivot]); //inorder[pivot] == prorder[rootIdx]
+        rootIdx++;
+        //main diff from prev post and in oreder solun
+        //left first then right
+        node->left = build(preorder, inorder, rootIdx, left, pivot-1, hmap);
+        node->right = build(preorder, inorder, rootIdx, pivot+1, right, hmap);         
+        return node;
+    }
+};
