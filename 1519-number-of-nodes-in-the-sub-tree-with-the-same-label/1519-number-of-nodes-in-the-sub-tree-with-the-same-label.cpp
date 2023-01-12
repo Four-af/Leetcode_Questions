81bdc68d61635088565c0f1820e7a211b6d6781b
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2023-01-12 23:27:29 +0530

    Time: 598 ms (82.96%), Space: 205.7 MB (74.07%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> function(vector<vector<int>> &adj, string &labels, int i, vector<int> &results){
+        vector<int> ans(26,0);
+        results[i] = 1;
+        ans[labels[i]-'a'] = 1;
+        for(int j=0; j!=adj[i].size();j++){
+            if(results[adj[i][j]]==0){
+                vector<int> temp = function(adj, labels, adj[i][j], results);
+                for(int k=0; k!=26 ; k++){
+                    ans[k]+=temp[k];
+                }   
+            }
+            
+        }
+        results[i] = ans[labels[i]-'a'];
+        return ans;
+    }
+//   vector<int> fun(vector<vector<int>> &adj, string &labels, int i,vector<int>&result){
+//     vector<int> ans(26, 0);
+//     result[i] = 1;
+//     ans[labels[i] - 'a'] = 1;
+    
+//     for(int j = 0; j != adj[i].size(); j++)
+//       if(!result[adj[i][j]]){
+//         vector<int> tmp = fun(adj, labels,adj[i][j],result);
+//         for(int k = 0; k != 26; k++) ans[k] += tmp[k];
+//       }
+    
+//     result[i] = ans[labels[i] - 'a'];
+    
+//     return ans;
+//   }
+  
+  vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+    vector<vector<int>> adj(n);
+    vector<int> result(n,0);
+    for(int i = 0; i != edges.size(); i++)
+     {adj[edges[i][0]].push_back(edges[i][1]);
+      adj[edges[i][1]].push_back(edges[i][0]);
+     }
+    
+    function(adj, labels, 0,result);
+    return result;
+  }
+};
