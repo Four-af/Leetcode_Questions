397d28852c8aff64e06f6a72c1e4e6ae02db30b9
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2023-03-11 00:04:32 +0530

    Time: 25 ms (56.31%), Space: 16.9 MB (8.14%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -23,23 +23,6 @@ public:
     }
 };
 
-//  vector<ListNode*> nodes;
-
-//     Solution(ListNode* head) {
-//         while(head!=NULL)
-//         {
-//             nodes.push_back(head);
-//             head=head->next;
-//         }
-//     }
-    
-//     int getRandom() {
-
-//       int n=nodes.size();
-//       int radn=rand()%n;
-//       return nodes[radn]->val;  
-      
-//     }
 
 /**
  * Your Solution object will be instantiated and called as such:
