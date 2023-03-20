7b45f79b22df6a3ba337d3b184a0dbd180ccc421
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2023-03-20 23:13:17 +0530

    Time: 21 ms (48.13%), Space: 20.3 MB (34.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int ans=0, m=flowerbed.size();
-        for(int i=0; i<m;i++){
-            if(flowerbed[i]==0 && (i==m-1 || flowerbed[i+1]==0) && (i==0 || flowerbed[i-1]==0)){
-                ans+=1;
-                flowerbed[i]=1;
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) 
+    {
+        // [0,0,1,0,1]
+        // for this type testcase we need to insert a 0 at beggining
+        flowerbed.insert(flowerbed.begin(),0);
+
+        // [1,0,0,0,1,0,0]
+        // for this type testcase we need to insert a 0 at end
+        flowerbed.push_back(0);
+
+        for(int i=1;i<flowerbed.size()-1;i++)
+        {
+            // if we have the triplet of zero then we can put a flower
+            if(flowerbed[i-1]+flowerbed[i]+flowerbed[i+1]==0)
+            {
+                n--;       // reduce flower numbers after putting one flower
+                i++;       // move to the next to next zero as we moved already by for loop i++
             }
-        }return ans>=n ? true : false;
+        }
+        return n<=0;
     }
 };
