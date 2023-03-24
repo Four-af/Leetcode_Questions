789afdf1867060a0ee45834b70afcb5618717eb6
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2023-03-24 16:39:26 +0530

    Time: 604 ms (29.06%), Space: 169.4 MB (17.72%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,52 +1,46 @@
+#define pb push_back
 class Solution {
 public:
-    int minScore(int n, vector<vector<int>>& roads) 
-    {
-        // Storing graph in a adjacency list
-        vector<vector<int>> adj[n+1];
-        for(int i=0;i<roads.size();i++)
-        {
-            adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
-            adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
+    int minScore(int n, vector<vector<int>>& r){
+        //making adj list
+        vector<vector<int>> a[n+1];
+        for(int i=0; i<r.size(); i++){
+            a[r[i][0]].pb({r[i][1],r[i][2]});
+            a[r[i][1]].pb({r[i][0],r[i][2]});
         }
-
-        // making a visited array for keep tracking of all the cities (we have visited or not)
-        // n = number of cities
-        vector<bool> vis(n+1,false);
-        vis[1] = true;                                  // mark source node as a visited city
-        
-        // BFS
+        //making a visited vector
+        vector<int> v(n+1,0);
+        v[1] = 1;
+        //traversing breadth first search... make queueueueueue
         queue<int> q;
+        //bcz cities r frm 1 to n ... means root is always 1
         q.push(1);
-
-        while(!q.empty())
-        {
-            int node = q.front();
+        //traverseeee
+        while(!q.empty()){
+            //get current node
+            int curr = q.front();
+            
+            //remove it from the queue and process it
             q.pop();
-
-            for(auto &it : adj[node])
-            {
-                if(!vis[it[0]])
-                {
-                    vis[it[0]] = true;
-                    q.push(it[0]);
+            for(auto &x : a[curr]){
+                if(!v[x[0]]){
+                    v[x[0]]=1;
+                    q.push(x[0]);
                 }
             }
         }
-
-
-        // Computing minimum distance from city 1 to n
+        
+        //gettin min distanec
         int ans = INT_MAX;
-        for(int i=0;i<roads.size();i++)
-        {
-            // minimum possible score out of all available paths
-            // if src and dest both are visited cities then try to make this our ans
-            if(vis[roads[i][0]] && vis[roads[i][1]])
-            {
-                ans = min(ans,roads[i][2]);
-            }
-        }
-
+         for(int i=0; i<r.size(); i++){
+              // minimum possible score out of all available paths
+          // if src and dest both are visited cities then try to make this our ans
+            if(v[r[i][0]] && v[r[i][1]])
+                ans = min(ans,r[i][2]);
+         }
         return ans;
+        
+              
     }
+        
 };
