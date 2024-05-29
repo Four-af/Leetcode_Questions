ef6d845e1818ff75e71cad31e6061914c6ab0ed6
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2024-05-29 23:16:27 +0530

    Time: 28 ms (60.82%), Space: 12.1 MB (92.46%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,6 +4,26 @@ public:
     //memo dp[n+1][amt+1]
     int dp[12+1][10000+1];
     
+    int tabula(vector<int>&coins,int n, int amt){
+        for (int i = 0; i <= n; i++){
+            for (int j = 0; j < amt + 1; j++){
+                if(i==0 || j==0){
+                    if(j == 0)
+                        dp[i][j] = 0;
+                    else dp[i][j] = INT_MAX-1;
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j < amt + 1; j++){
+                if(coins[i-1]>j) dp[i][j] = 0 + dp[i-1][j];
+                else
+                    dp[i][j] = min(0 + dp[i-1][j], 1+dp[i][j-coins[i-1]]);
+            }
+        }
+        return dp[n][amt];
+    }
+    
     int recur(vector<int>&coins, int idx, int amt){
         if(idx>=coins.size() || amt<=0){
             if(amt == 0){
@@ -41,7 +61,9 @@ public:
     //     1 2 3
     //take not take
          memset(dp, -1, sizeof(dp));
-        int ans = recur(coins, 0, amount);
+        // int ans = recur(coins, 0, amount);
+        int n=coins.size();
+         int ans = tabula(coins, n, amount);
         if(ans == INT_MAX -1) return -1;
         return ans;
         
