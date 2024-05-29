424ede4ac1b82f27f9ad673727c05feb31ef5c60
Aiman Fatima <83394414+Four-af@users.noreply.github.com>
2024-05-29 11:46:13 +0530

    Time: 76 ms (62.88%), Space: 70.3 MB (29.33%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int ans = INT_MIN, sum = 0, idx = -1, strt = -1, end = -1;
+        int ele = INT_MIN;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            ans = max(ans, sum);
+            ele = max(ele, nums[i]);
+            if (sum < 0)
+                sum = 0;
+        }
+        // if (sum == 0)
+        //     ans = ele;
+        return ans;
+    }
+};
